a78208ac87164670e9675af803814641aa4681b3
Javier Beneito Barquero <jbbarquero@gmail.com>
2015-10-22 05:49:33 +0200

    OAuth2 Server empty (just created the project)

---
+++ src/main/java/com/malsolo/mercurius/oauth2/server/MercuriusSsoOauthServerApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.mercurius.oauth2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuriusSsoOauthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercuriusSsoOauthServerApplication.class, args);
+    }
+}
